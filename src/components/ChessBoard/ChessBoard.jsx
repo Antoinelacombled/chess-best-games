36bbe150f1389871e3c0fcdb5316276d0ec8d09d
Antoinelacombled <antoine.j.lacombled@gmail.com>
2023-10-26 16:39:21 +0200

    axis ok pieces ok

--- src/components/ChessBoard/ChessBoard.jsx
+++ src/components/ChessBoard/ChessBoard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from "react";
 import "./ChessBoard.scss";
 import Tile from "../Tile/Tile";
 import pawnLight from "../../assets/chess-pawn-regular-light.svg";
@@ -45,6 +46,61 @@ pieces.push({ image: queenLight, x: 3, y: 0 });
 pieces.push({ image: kingLight, x: 4, y: 0 });
 
 function Chessboard() {
+  let activePiece = null;
+
+  function grabPiece(e) {
+    const element = e.target;
+    if (element.classList.contains("chess-piece")) {
+      console.log(e);
+
+      const x = e.clientX - 22.5;
+      const y = e.clientY - 22.5;
+      element.style.position = "absolute";
+      element.style.left = `${x}px`;
+      element.style.top = `${y}px`;
+
+      activePiece = element;
+    }
+  }
+
+  function movePiece(e) {
+    const chessboard = chessboardRef.current;
+    if (activePiece && chessboard) {
+      const minX = chessboardRef.offsetLeft - 25;
+      const minY = chessboardRef.offsetTop - 25;
+      const maxX = chessboard.offsetLeft + chessboard.clientWidth - 35;
+      const maxY = chessboard.offsetTop + chessboard.clientHeight - 40;
+      const x = e.clientX - 22.5;
+      const y = e.clientY - 22.5;
+      activePiece.style.position = "absolute";
+      // activePiece.style.left = `${x}px`;
+      // activePiece.style.top = `${y}px`;
+
+      if (x < minX) {
+        activePiece.style.left = `${minX}px`;
+      } else if (x > maxX) {
+        activePiece.style.left = `${maxX}px`;
+      } else {
+        activePiece.style.left = `${x}px`;
+      }
+
+      if (y < minY) {
+        activePiece.style.top = `${minY}px`;
+      } else if (y > maxY) {
+        activePiece.style.top = `${maxY}px`;
+      } else {
+        activePiece.style.top = `${y}px`;
+      }
+    }
+  }
+
+  function dropPiece(e) {
+    if (activePiece) {
+      activePiece = null;
+    }
+  }
+
+  const chessboardRef = useRef(null);
   let board = [];
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
@@ -57,17 +113,21 @@ function Chessboard() {
         }
       });
 
-      board.push(
-        <Tile
-          key={horizontalAxis[i] + verticalAxis[j]}
-          image={image}
-          number={number}
-        />
-      ); // Ajout d'une clé unique pour chaque tuile
+      board.push(<Tile key={`${j}, ${i}`} image={image} number={number} />); // Ajout d'une clé unique pour chaque tuile
     }
   }
 
-  return <div id="chessboard">{board}</div>;
+  return (
+    <div
+      onMouseMove={(e) => movePiece(e)}
+      onMouseDown={(e) => grabPiece(e)}
+      onMouseUp={(e) => dropPiece(e)}
+      id="chessboard"
+      ref={chessboardRef}
+    >
+      {board}
+    </div>
+  );
 }
 
 export default Chessboard;
